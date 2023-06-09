114a3b09fffb73d9cda7ad8e4b566d6f9adbfe9c
sksksk705 <sksksk705@hanyang.ac.kr>
2023-06-09 13:42:00 +0900

    color constant 추가

--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const MainPage = () => {
-
   return (
-    <div className='w-screen h- flex items-end'>
+    <div className="w-screen h- flex items-end">
       <NavLink to="/select">
-        <button className='flex w-80 h-14 justify-center items-center bg-my-green rounded-xl'>
+        <button className="flex w-80 h-14 justify-center items-center bg-p1 rounded-xl">
           경로 설정
         </button>
       </NavLink>
--- src/pages/Splash/SplashPage.jsx
+++ src/pages/Splash/SplashPage.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { ReactComponent as Logo } from '../../assets/images/logo.svg';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/images/logo.svg";
 
 const SplashPage = () => {
   const navigate = useNavigate();
@@ -12,12 +12,14 @@ const SplashPage = () => {
   }, [navigate]);
 
   return (
-    <div className='flex w-screen h-screen flex-col justify-between bg-my-green'>
+    <div className="flex w-screen h-screen flex-col justify-between bg-p1">
       <div></div>
-      <div className='flex flex-col items-center'>
-        <p className='p1b text-white'>모든 이동이 즐거움으로 변한다</p>
-        <p className='font-jalnan text-white' style={{fontSize: "36px"}}>모두의 지하철</p>
-        <Logo className='w-24 h-24' />
+      <div className="flex flex-col items-center">
+        <p className="p1b text-white">모든 이동이 즐거움으로 변한다</p>
+        <p className="font-jalnan text-white" style={{ fontSize: "36px" }}>
+          모두의 지하철
+        </p>
+        <Logo className="w-24 h-24" />
       </div>
       <div></div>
     </div>
