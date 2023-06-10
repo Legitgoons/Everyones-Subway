985aa708caa24b02d48a4bbc5d5e159d83f4de84
sksksk705 <sksksk705@hanyang.ac.kr>
2023-06-11 01:56:58 +0900

    색 추가

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,10 +1,36 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class NotFound extends Component {
   render() {
     return (
       <div>
         <h1>404 NotFound</h1>
+        <div
+          className='hidden 
+bg-l1
+bg-l2
+bg-l3
+bg-l4
+bg-l5
+bg-l6
+bg-l7
+bg-l8
+bg-l9
+bg-l공
+bg-l중
+bg-l춘
+bg-l신
+bg-l림
+bg-l용
+bg-l서
+bg-l김
+bg-l의
+bg-l우
+bg-l인
+bg-l천
+bg-l강
+bg-l분'
+        ></div>
       </div>
     );
   }
