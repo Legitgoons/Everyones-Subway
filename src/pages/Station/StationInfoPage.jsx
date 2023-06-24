ba9b1e37ca0e553c0342ec243f41a55ade14e523
Uichan Lee <cksxkr5193@naver.com>
2023-06-25 00:26:19 +0900

    code 정리

---
+++ src/pages/Station/StationInfoPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StationInfo from '../../components/StationInfo/StationInfo'
+
+const StationInfoPage = () => {
+  return (
+    <div>
+      <StationInfo />
+    </div>
+  );
+};
+
+export default StationInfoPage;
